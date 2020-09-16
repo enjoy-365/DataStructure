ea55998cc49fbd823df71deb084d5bda95ae3dce
enjoy-365 <tlgus0812@hanyang.ac.kr>
2020-09-16 23:54:59 +0900

    start coding queue

--- queue/array/queue.c
+++ queue/array/queue.c
@@ -1,7 +1,24 @@
-#include "queue.h"
+//#include "queue.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+
+typedef struct {
+    int key;
+}element;
+element queue[MAX_QUEUE_SIZE];
+int rear = -1;
+int front = 0;
+
+void Enqueue(element item, element* queue){
+    if(rear == MAX_QUEUE_SIZE -1){
+        printf("queue is full\n");
+    }
+    queue[++rear] = item;
+}
+
 
 int main(void){
 
--- queue/array/queue.h
+++ queue/array/queue.h
@@ -2,4 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_QUEUE_SIZE 100
 
+typedef struct {
+    int key;
+}element;
+
+element queue[MAX_QUEUE_SIZE];
+int rear = -1;
+int front = 0;
