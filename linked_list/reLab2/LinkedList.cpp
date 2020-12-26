06412d344cb836978e1b984c0aa6865342e96961
enjoy-365 <tlgus0812@hanyang.ac.kr>
2020-12-26 16:14:27 +0900

    file i/o is needed

--- linked_list/reLab2/LinkedList.cpp
+++ linked_list/reLab2/LinkedList.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef struct Node* PtrToNode;
 typedef PtrToNode List;
@@ -14,14 +15,15 @@ struct Node {
 	ElementType element;
 	PtrToNode next;
 };
-void Insert(ElementType X, List L, Position P);
-void Delete(ElementType X, List L);
+int Insert(ElementType X, List L);
+int Delete(ElementType X, List L);
 Position Find(ElementType X, List L);
 void PrintList(List L, FILE* fp2);
-List MakeEmpty(List L);
+List MakeEmpty();
 int IsEmpty(List L);
 int IsLast(Position P, List L);
 void DeleteList(List L);
+char* LeftTrim(char* s);
 
 int main(void) {
 	FILE* fp1 = fopen("input.txt", "r");
@@ -33,30 +35,89 @@ int main(void) {
 	int id;
 	char name[20];
 
+	List h = MakeEmpty();
+
 	while (fgets(line, 100, fp1) != NULL) {
 		sscanf(line, "%c", &type);
 		if (type == 'i') {
 			sscanf(line, "%c%d%[^\n]s", &check, &id, name);
+			char* trimmed = LeftTrim(name);
+			ElementType temp;
+			temp.studentID = id;
+			temp.studentName = (char*)malloc(strlen(trimmed) + 1);
+			strcpy(temp.studentName, trimmed);
+			if (Insert(temp, h)) {
+				printf("Insertion Success : %d\n", temp.studentID);
+				Position P = h->next;
+				printf("Current List >");
+				printf(" %d %s", P->element.studentID, P->element.studentName);
+				P = P->next;
+				while (P != NULL) {
+					printf("-%d %s", P->element.studentID, P->element.studentName);
+					P = P->next;
+				}
+				printf("\n");
+			}
+			else {
+				printf("Insertion Failed. ID %d already exists.\n", temp.studentID);
+			}
+
 			/*printf("%c ", check);
 			printf("%d ", id);
 			printf("%s ", name);
 			printf("\n");*/
 		}
 		else if (type == 'd') {
-			sscanf(line, "%d", &id);
+			sscanf(line, "%c%d", &type, &id);
+			/*printf("target deletion: %d\n", id);
+			printf("target type: %c\n", type);
+			*/
+			ElementType temp;
+			temp.studentID = id;
+			if (Delete(temp, h)) {
+				printf("Deletion Success : %d\n", id);
+				Position P = h->next;
+				printf("Current List > ");
+				while (P != NULL) {
+					printf("%d %s", P->element.studentID, P->element.studentName);
+					P = P->next;
+					if (P != NULL) {
+						printf("-");
+					}
+				}
+				printf("\n");
+			}
+			else {
+				printf("Deletion Failed : Studdent ID %d is not in the list.\n", id);
+			}
 			/*printf("%d ", id);
 			printf("\n");*/
 		}
 		else if (type == 'f') {
+			sscanf(line, "%c%d", &type, &id);
+			ElementType temp;
+			temp.studentID = id;
+			Position res = Find(temp, h);
+			if (res != NULL) {
+				printf("Find Success : %d %s\n", id, res->element.studentName);
+			}
+			else {
+				printf("Find %d Failed. There is no student ID\n", id);
+			}
 
-			sscanf(line, "%d", &id);
 			/*printf("%d ", id);
 			printf("\n");*/
 		}
-		else
+		else {
 			// type이 p일 때이다.
-			printf("%c ", type);
-		printf("\n");
+			Position P = h->next;
+			printf("-----LIST-----\n");
+			while (P != NULL) {
+				printf("%d %s\n", P->element.studentID, P->element.studentName);
+				P = P->next;
+			}
+			printf("--------------");
+		}
 	}
 
 
@@ -65,8 +126,8 @@ int main(void) {
 
 }
 
-List MakeEmpty(List L) {
-	L = (List)malloc(sizeof(struct Node));
+List MakeEmpty() {
+	List L = (List)malloc(sizeof(struct Node));
 	L->next = NULL;
 
 	return L;
@@ -92,4 +153,65 @@ void PrintList(List L, FILE* fp2) {
 		P = P->next;
 	}
 	fprintf(fp2, "--------------");
+}
+int Insert(ElementType X, List L) {
+	if (Find(X, L) != NULL) return 0;
+	List temp = (List)malloc(sizeof(struct Node));
+	temp->element.studentID = X.studentID;
+	temp->element.studentName = (char*)malloc(strlen(X.studentName) + 1);
+	strcpy(temp->element.studentName, X.studentName);
+
+	Position P = L;
+	while (P->next != NULL && P->next->element.studentID < X.studentID) {
+		P = P->next;
+	}
+	//이제 P뒤에다가 새로 만든 temp 노드를 끼워넣는다.
+
+	temp->next = P->next;
+	P->next = temp;
+
+	return 1;
+}
+int Delete(ElementType X, List L) {
+	Position target = Find(X, L);
+	if (target != NULL) {
+		Position prev = L;
+		while (prev->next != NULL && prev->next->element.studentID != X.studentID) {
+			prev = prev->next;
+		}
+
+		prev->next = target->next;
+		free(target);
+		target = NULL;
+
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+void DeleteList(List L) {
+	Position P, tmp;
+	P = L->next;
+	L->next = NULL;
+
+	while (P != NULL) {
+		tmp = P->next;
+		free(P);
+		P = tmp;
+	}
+}
+
+char* LeftTrim(char* s) {
+	char* left = s;
+	while (*left != '\0') {
+		if (isspace(*left)) {
+			left++;
+		}
+		else {
+			s = left;
+			break;
+		}
+	}
+	return s;
 }
