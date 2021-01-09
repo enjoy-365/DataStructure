574b730caadc7207445a1f580e497040d2f79a3a
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-01-09 21:00:50 +0900

    stack array version is done

--- stack/Lab3-1/Stackarray.cpp
+++ stack/Lab3-1/Stackarray.cpp
@@ -1,5 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#define EmptyTOS (-1)
+#define LIMIT 99
+
+typedef struct StackRecord* Stack;
+
+struct StackRecord {
+	int capacity;
+	int topOfStack;
+	int* array;
+};
+
+Stack CreateStack(int maxElements) {
+	Stack S;
+	S = (Stack)malloc(sizeof(struct StackRecord));
+	S->array = (int*)malloc(sizeof(int) * maxElements);
+	S->capacity = maxElements;
+	S->topOfStack = EmptyTOS;
+
+	return S;
+}
+
+int IsFull(Stack S) {
+	if (S->capacity == S->topOfStack) {
+		return 1;
+	}
+	else return 0;
+}
+
+void Push(FILE* fp2, int x, Stack S) {
+	if (IsFull(S)) {
+		fprintf(fp2, "Full\n");
+	}
+	else {
+		S->array[++S->topOfStack] = x;
+
+	}
+}
+
+int IsEmpty(Stack S) {
+	if (S->topOfStack < 0) return 1;
+	else return 0;
+}
+
+int Top(Stack S) {
+	if (!IsEmpty(S)) return S->array[S->topOfStack];
+	else return 0;
+}
+
+void Pop(FILE* fp2, Stack S) {
+	if (IsEmpty(S)) {
+		fprintf(fp2, "Empty\n");
+	}
+	else {
+		fprintf(fp2, "%d\n", Top(S));
+		S->topOfStack--;
+	}
+}
+
+
 
 int main() {
 	int cnt;
@@ -8,19 +68,22 @@ int main() {
 	char type[6];
 	int element;
 
+	Stack S = CreateStack(LIMIT);
+
 	fscanf(fp1, "%d", &cnt);
-	printf("%d\n", cnt);
+	//printf("%d\n", cnt);
 
 	while (cnt--) {
 		fscanf(fp1, "%s", type);
-		printf("%s\n", type);
+
+		//printf("%s\n", type);
 
 		if (strcmp(type, "push") == 0) {
 			fscanf(fp1, "%d", &element);
-			printf("%d\n", element);
+			Push(fp2, element, S);
 		}
 		else {
-			printf("\n");
+			Pop(fp2, S);
 		}
 	}
 
