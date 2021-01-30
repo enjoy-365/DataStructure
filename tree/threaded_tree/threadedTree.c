2f5c2ea32800d81d6c5e73f6ff42c27ba358842a
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-01-30 23:39:35 +0900

    making threaded_tree

--- tree/threaded_tree/threadedTree.c
+++ tree/threaded_tree/threadedTree.c
@@ -3,7 +3,19 @@
 #include <string.h>
 
 int main() {
-	
+	int cnt;
+	char c;
+	FILE* fp1, fp2;
+	fopen(fp1,"input.txt", "r");
+	fopen(fp2, "output.txt", "w");
+
+	fscanf(fp1, "%d", &cnt);
+	while (cnt--) {
+		fscanf(fp1, "%c", &c);
+	}
+
+	fclose(fp1);
+	fclose(fp2);
 
 	return 0;
 }
