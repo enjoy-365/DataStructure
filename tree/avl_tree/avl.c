22eed26e5b39be0fa8456c55dca36179c2202fc2
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-02-10 23:45:26 +0900

    made printdebug function

--- tree/avl_tree/avl.c
+++ tree/avl_tree/avl.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#define DEBUG
+
+void PrintDebug(const char* format, ...) {
+#ifdef DEBUG
+	va_list ap;
+	va_start(ap, format);
+
+	vprintf(format, ap);
+
+	va_end(ap);
+#endif
+}
+
 struct AVLNode;
 typedef struct AVLNode* Position;
 typedef struct AVLNode* AVLTree;
@@ -23,9 +36,12 @@ Position DoubleRotateWithRight(Position K);
 int main() {
 	FILE* fp1, * fp2;
 	fp1 = fopen("input.txt", "r");
-	fp2 = foepn("output.txt", "w");
+	fp2 = fopen("output.txt", "w");
 
-
+	int data;
+	while (fscanf(fp1, "%d", &data) != EOF) {
+		PrintDebug("%d ", data);
+	}
 
 
 	fclose(fp1);
@@ -34,6 +50,7 @@ int main() {
 	return 0;
 }
 
+/*
 int Height(Position P) {
 
 }
@@ -51,4 +68,4 @@ Position DoubleRotateWithLeft(Position K) {
 }
 Position DoubleRotateWithRight(Position K) {
 
-}
+}*/
