78f550962be6159121d5cf0c4c4e72fb75001aad
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-02-12 03:07:23 +0900

    avl deletion complete but, when 1 element is left runtime error occurs for calling NULL->element

--- tree/avl_tree/avl.c
+++ tree/avl_tree/avl.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-//#define DEBUG
+#define DEBUG
 
 void PrintDebug(const char* format, ...) {
 #ifdef DEBUG
@@ -28,12 +28,16 @@ struct AVLNode {
 
 int Height(Position P);
 int Max(int a, int b);
+int getBalance(AVLTree T);
 AVLTree Insert(int x, AVLTree T, FILE* fp2, int* chk);
 Position SingleRotateWithLeft(Position K);
 Position SingleRotateWithRight(Position K);
 Position DoubleRotateWithLeft(Position K);
 Position DoubleRotateWithRight(Position K);
 void PrintInorder(AVLTree root, FILE* fp2);
+void DebugInorder(AVLTree root);
+Position Delete(int x, AVLTree T, FILE* fp2, int* chk);
+Position FindMin(AVLTree T);
 
 int main() {
 	FILE* fp1, * fp2;
@@ -44,12 +48,31 @@ int main() {
 	int data;
 	AVLTree root = NULL;
 	while (fscanf(fp1, "%d", &data) != EOF) {
-		PrintDebug("%d ", data);
+		//PrintDebug("%d ", data);
 		root = Insert(data, root, fp2, &chk);
 		if (chk) {
+			DebugInorder(root);
+			PrintDebug("\n");
 			PrintInorder(root, fp2);
 			fprintf(fp2, "\n");
 		}
+	}
+	char cmd;
+	printf("delete for 'd' || quit to 'q'\n");
+	fprintf(fp2, "---------deletion start---------\n");
+	while (1) {
+		scanf("%c", &cmd);
+		if (cmd == 'd') {
+			scanf("%d", &data);
+			PrintDebug("root element before deletion: %d(%d)\n", root->element, root->h);
+			root = Delete(data, root, fp2, &chk);
+			PrintDebug("root element after deletion: %d(%d)\n", root->element, root->h);
+			DebugInorder(root);
+			PrintDebug("\n");
+			PrintInorder(root, fp2);
+			fprintf(fp2, "\n");
+		}
+		else if (cmd == 'q') break;
 	}
 
 
@@ -69,6 +92,10 @@ int Height(Position P) {
 }
 int Max(int a, int b) {
 	return a > b ? a : b;
+}
+int getBalance(AVLTree T) {
+	if (T == NULL) return 0;
+	return Height(T->left) - Height(T->right);
 }
 AVLTree Insert(int x, AVLTree T, FILE* fp2, int* chk) {
 	if (T == NULL) {
@@ -153,7 +180,92 @@ Position DoubleRotateWithRight(Position K3) {
 void PrintInorder(AVLTree root, FILE* fp2) {
 	if (root) {
 		PrintInorder(root->left, fp2);
-		fprintf(fp2, "%d(%d)", root->element, root->h);
+		fprintf(fp2, "%d(%d) ", root->element, root->h);
 		PrintInorder(root->right, fp2);
 	}
+}
+void DebugInorder(AVLTree root) {
+#ifdef DEBUG
+	if (root) {
+		DebugInorder(root->left);
+		PrintDebug("%d(%d) ", root->element, root->h);
+		DebugInorder(root->right);
+	}
+#endif
+}
+Position Delete(int x, AVLTree T, FILE* fp2, int* chk) {
+	AVLTree temp;
+	if (T == NULL) return T;
+	else if (x < T->element) {
+		PrintDebug("called Delete(%d)\n", T->element);
+		T->left = Delete(x, T->left, fp2, chk);
+
+		//PrintDebug("after called Delete(%d below the %d)\n", T->element, T->left->element);
+	}
+	else if (x > T->element) {
+		PrintDebug("called Delete(%d)\n", T->element);
+		T->right = Delete(x, T->right, fp2, chk);
+		//PrintDebug("after called Delete(%d)\n", T->element);
+		//PrintDebug("after called Delete(%d below the %d)\n", T->element, T->right->element);
+	}
+	else if (T->right && T->left) {
+		temp = FindMin(T->right);
+		PrintDebug("FindMin result %d\n", temp->element);
+		T->element = temp->element;
+		PrintDebug("called Delete(%d)\n", T->element);
+		T->right = Delete(temp->element, T->right, fp2, chk);
+		//PrintDebug("after called Delete(%d)\n", T->element);
+		//PrintDebug("after called Delete(%d below the %d)\n", T->element, T->right->element);
+	}
+	else if (T->right == NULL || T->left == NULL) {
+		temp = T;
+		if (T->left == NULL) {
+			PrintDebug("delete %d\n", temp->element);
+			T = T->right;
+		}
+		else if (T->right == NULL) {
+			PrintDebug("delete %d\n", temp->element);
+			T = T->left;
+		}
+		free(temp);
+	}
+
+	if (T == NULL) return T;
+
+	T->h = Max(Height(T->left), Height(T->right)) + 1;
+	int balance = getBalance(T);
+
+	if (balance == 2) {
+		if (getBalance(T->left) >= 0) {
+			PrintDebug("x is %d, left element is %d\n", x, T->left->element);
+			PrintDebug("SingleRotateWithLeft(%d)\n", T->element);
+			T = SingleRotateWithLeft(T);
+		}
+		else {
+			PrintDebug("x is %d, left element is %d\n", x, T->left->element);
+			PrintDebug("DoubleRotateWithLeft(%d)\n", T->element);
+			T = DoubleRotateWithLeft(T);
+		}
+	}
+	else if (balance == -2) {
+		if (getBalance(T->right) <= 0) {
+			PrintDebug("x is %d, right element is %d\n", x, T->right->element);
+			PrintDebug("SingleRotateWithRight(%d)\n", T->element);
+			T = SingleRotateWithRight(T);
+		}
+		else {
+			PrintDebug("x is %d, right element is %d\n", x, T->right->element);
+			PrintDebug("DoubleRotateWithRight(%d)\n", T->element);
+			T = DoubleRotateWithRight(T);
+		}
+	}
+	T->h = Max(Height(T->left), Height(T->right)) + 1;
+	return T;
+}
+Position FindMin(AVLTree T) {
+	if (T == NULL) return T;
+	else if (T->left == NULL) {
+		return T;
+	}
+	return FindMin(T->left);
 }
