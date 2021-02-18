f82d032619cb54042ebb464b04c7d358e0f082f2
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-02-19 01:33:58 +0900

    max heap insertion finished, try delete max

--- tree/heap/maxheap.c
+++ tree/heap/maxheap.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
-#define HEAPSIZE 100
+#define DEBUG
+
+void PrintDebug(const char* format, ...) {
+#ifdef DEBUG
+	va_list ap;
+	va_start(ap, format);
+	vprintf(format, ap);
+	va_end(ap);
+#endif
+}
 
 typedef int ElementType;
 typedef struct HeapStruct* PriorityQueue;
@@ -11,10 +21,11 @@ struct HeapStruct {
 	ElementType* elements;
 };
 
-PriorityQueue InitHeap();
+PriorityQueue InitHeap(int heapsize);
 void Insert(ElementType x, PriorityQueue h, FILE* fp2);
-void Find(ElementType x, PriorityQueue h);
-void Print(PriorityQueue h);
+int Find(ElementType x, PriorityQueue h);
+//int FindRecursive(ElementType x, PriorityQueue h);
+void Print(PriorityQueue h, FILE* fp2);
 int IsFull(PriorityQueue h);
 
 int main() {
@@ -23,6 +34,32 @@ int main() {
 	fp1 = fopen("input.txt", "r");
 	fp2 = fopen("output.txt", "w");
 
+	int heapsize;
+	fscanf(fp1, "%d", &heapsize);
+	PriorityQueue h = InitHeap(heapsize);
+
+	char cmd;
+	int num;
+	while (fscanf(fp1, "%c", &cmd) != EOF) {
+		if (cmd == 'i') {
+			fscanf(fp1, "%d", &num);
+			Insert(num, h, fp2);
+			PrintDebug("after insert %d, h->size == %d\n", num, h->size);
+		}
+		else if (cmd == 'f') {
+			fscanf(fp1, "%d", &num);
+			if (Find(num, h)) {
+				fprintf(fp2, "%d is in the heap\n", num);
+			}
+			else {
+				fprintf(fp2, "%d is not in the heap\n", num);
+			}
+		}
+		else if (cmd == 'p') {
+			Print(h, fp2);
+		}
+	}
+
 
 	fclose(fp1);
 	fclose(fp2);
@@ -30,11 +67,11 @@ int main() {
 	return 0;
 }
 
-PriorityQueue InitHeap() {
+PriorityQueue InitHeap(int heapsize) {
 	PriorityQueue h = (PriorityQueue)malloc(sizeof(struct HeapStruct));
 	h->size = 0;
-	h->capacity = HEAPSIZE;
-	h->elements = (ElementType*)malloc(sizeof(int) * h->capacity);
+	h->capacity = heapsize;
+	h->elements = (ElementType*)malloc(sizeof(int) * h->capacity + 1);
 
 	return h;
 }
@@ -43,17 +80,33 @@ void Insert(ElementType x, PriorityQueue h, FILE* fp2) {
 		fprintf(fp2, "heap is full\n");
 		return;
 	}
-	int i = 0;
-	for (i = ++h->size; h->elements[i / 2] < x; i /= 2) {
+	if (Find(x, h) == 1) {
+		fprintf(fp2, "%d is already in the heap\n", x);
+		return;
+	}
+	int i;
+	for (i = ++h->size; i / 2 > 0 && h->elements[i / 2] < x; i /= 2) {
 		h->elements[i] = h->elements[i / 2];
 	}
 	h->elements[i] = x;
+	fprintf(fp2, "insert %d\n", x);
 }
-void Find(ElementType x, PriorityQueue h) {
-
+int Find(ElementType x, PriorityQueue h) {
+	for (int i = 1; i <= h->size; i++) {
+		if (h->elements[i] == x) {
+			return 1;
+		}
+	}
+	return 0;
 }
-void Print(PriorityQueue h) {
+/*int FindRecursive(ElementType x, PriorityQueue h) {
 
+}*/
+void Print(PriorityQueue h, FILE* fp2) {
+	for (int i = 1; i <= h->size; i++) {
+		fprintf(fp2, "%d ", h->elements[i]);
+	}
+	fprintf(fp2, "\n");
 }
 int IsFull(PriorityQueue h) {
 	return h->size == h->capacity;
