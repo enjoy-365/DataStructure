12bfef07fc028b65c3cdf78849c7a2f8ac081a0f
enjoy-365 <tlgus0812@hanyang.ac.kr>
2021-02-28 23:22:08 +0900

    btree start making functions

--- tree/btree/bTree.c
+++ tree/btree/bTree.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define order 3
+
+typedef struct B_node {
+	int n_keys;
+	B_node* child[order];
+	int key[order - 1];
+};
+
+void Insert(int key);
+void Inorder(struct B_node* ptr);
 
 int main(void) {
+	FILE* fp1, * fp2;
 
+	fp1 = fopen("input.txt", "r");
+	fp2 = fopen("output.txt", "w");
+
+	char cmd, n;
+	while (fscanf(fp1, "%c", &cmd) != EOF) {
+		if (cmd == 'i') {
+			fscanf(fp1, "%d", &n);
+		}
+		else if (cmd == 'p') {
+
+		}
+		else {
+			fprintf(fp2, "undefined command\n");
+		}
+	}
+
+	fclose(fp1);
+	fclose(fp2);
 
 	return 0;
+}
+void Insert(int key) {
+
+}
+void Inorder(struct B_node* ptr) {
+
 }
